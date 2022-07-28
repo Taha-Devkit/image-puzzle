c4f81cd8806d27d20a7b3237e8e4edb0fe48c75f
Taha-Devkit <49005988+Taha-Devkit@users.noreply.github.com>
2022-07-28 23:39:06 +0100

    Update game-configs.js

--- src/game-configs.js
+++ src/game-configs.js
@@ -1,5 +1,10 @@
 export const gameConfigs = [
     {
+        'name': '3x3 Puzzle',
+        'size': 3,
+        'highscorelistid': process.env.REACT_APP_HIGHSCORELISTID_4x4
+    },
+    {
         'name': '4x4 Puzzle',
         'size': 4,
         'highscorelistid': process.env.REACT_APP_HIGHSCORELISTID_4x4
